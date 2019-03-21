7ef62c7c6997ac074da0ed68f589a4296b8c3bb5
Linus Heckemann <git@sphalerite.org>
2019-03-21 13:47:32 +0100

    Add nifty_seg_git (development version)

--- default.nix
+++ default.nix
@@ -3,5 +3,6 @@ rec {
   irtk = callPackage ./irtk.nix {};
   nifty_reg = callPackage ./nifty_reg.nix {};
   nifty_seg = callPackage ./nifty_seg.nix {};
+  nifty_seg_git = callPackage ./nifty_seg_git.nix {};
   pincram = callPackage ./pincram.nix { inherit irtk; };
 }
---
+++ nifty_seg_git.nix
@@ -0,0 +1,15 @@
+{ stdenv, lib, fetchFromGitHub, pkgconfig, cmake, zlib, eigen }:
+stdenv.mkDerivation rec {
+  version = "git";
+  name = "nifty_seg-${version}";
+  src = fetchFromGitHub {
+    owner = "KCL-BMEIS";
+    repo = "NiftySeg";
+    rev = "87b29252443c37c5ac3827764256f21ad8d99e3f";
+    sha256 = "01iyyf8j2p3m2siycdxscs3ss1b0clqnpasbxyggmsk87507xvii";
+  };
+  buildInputs = [ cmake eigen zlib ];
+  nativeBuildInputs = [ pkgconfig ];
+  enableParallelBuilding = true;
+  cmakeFlags = ["-DBUILD_Z=OFF"];
+}
