cb715bf20dff33aa9ea303e9aa1b3d9e7fa2eabe
Linus Heckemann <git@sphalerite.org>
2019-04-28 10:48:02 +0200

    Fix nifty_seg_git, remove pincram for now

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ self = {
   kwstyle = callPackage ./kwstyle.nix {};
   nifty_reg = callPackage ./nifty_reg.nix {};
   nifty_seg = callPackage ./nifty_seg.nix {};
-  nifty_seg_git = nifty_seg.overrideAttrs (o: {
+  nifty_seg_git = self.nifty_seg.overrideAttrs (o: {
     name = "nifty_seg-git";
     src = fetchFromGitHub {
       owner = "KCL-BMEIS";
@@ -17,7 +17,6 @@ self = {
       sha256 = "01iyyf8j2p3m2siycdxscs3ss1b0clqnpasbxyggmsk87507xvii";
     };
   });
-  pincram = callPackage ./pincram.nix {};
   xmedcon = callPackage ./xmedcon.nix {};
 };
 in self
